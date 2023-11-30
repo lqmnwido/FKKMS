6494d1498e3e885b3157455b3aaafac4b8f3d4d4
lqmnwido <47884472+lqmnwido@users.noreply.github.com>
2023-11-30 11:55:19 +0800

    Version 0.1(Alpha)

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,11 @@
+</main>
+
+    <!-- Footer -->
+    <footer>
+
+    </footer>
+
+    <script src="{{asset('js/bootstrap.budle.min.js')}}"></script>
+    <script src="{{asset('js/jquery.js')}}"></script>
+</body>
+</html>
